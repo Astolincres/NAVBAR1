4b17ff42fce8ba8ac47af9fb1cc4dceab2a98d67
Astolincres <81331916+Astolincres@users.noreply.github.com>
2021-12-13 17:45:48 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,13 +2,30 @@ const sections = document.querySelector('.navbar__div');
 const sectionText = document.querySelector('.section__text');
 const sectionElements = Array.from(sections.children);
 
+
 sections.addEventListener('click', (e)=> {
   if (!e.target.classList.contains('link')) {
-    return
+    return;
   }
   sectionElements.forEach((section) => {
     section.classList.remove('here');
   })
-  e.target.classList.add('here')
+  e.target.classList.add('here');
   sectionText.textContent = e.target.textContent;
+  
 });
+
+
+
+const check = ()=> {
+  sectionElements.forEach((element) => {
+    if(window.location.hash === element.hash) {
+      element.classList.add('here');
+      sectionText.textContent = element.textContent;
+      return
+    };
+  });
+};
+
+
+check();
